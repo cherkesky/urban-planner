a370ae10a3fa58b50c21fa885d4f0db93f1dc06c
Cherkesky <guy@cherkesky.com>
2020-04-16 20:03:33 -0500

    finished the Building class

--- Building.cs
+++ Building.cs
@@ -4,18 +4,43 @@ namespace Planner
 {
   public class Building
   {
+    /*
+    constructor
+    */
+    public Building(string address)
+    {
+      _address = address;
+    }
+    /*
+    fields
+    */
     private string _designer;
     private DateTime _dateConstructed;
     private string _address;
     private string _owner;
-
-    public int Stories {get; set;}
-    public double Width {get; set;}
-    public double Depth {get; set;}
-    public double Volume {
-      get{
+    /*
+     properties
+     */
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get
+      {
         return Width * Depth;
       }
     }
-}
+    /*
+    methods
+    */
+    public void Construct(){
+      _dateConstructed = DateTime.Now;
+    }
+    public void Purchase(string owner){
+      _owner = owner;
+    }
+
+
+  }
 }
