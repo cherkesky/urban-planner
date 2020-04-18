54e548b14b5f4868cf596e23a6ef22972fb54374
Cherkesky <guy@cherkesky.com>
2020-04-17 19:08:38 -0500

    added city class. added fields

---
+++ City.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Planner
+{
+  public class City{
+    // constructor
+    public City(){
+
+    }
+    // fields
+  private string _city;
+  private string _mayor;
+  private double _year;
+  private List<Building> _allBuildings;
+
+
+
+
+  }
+
+
+}
